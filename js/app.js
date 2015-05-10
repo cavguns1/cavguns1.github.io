5fe71587a89c42725f4d23f67ab43c08a294a330
Chris Terrell <cav_guns@hotmail.com>
2015-05-10 15:28:06 -0700

    changed another image link for app.js file

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var backgrounds = [
     "url(http://res.cloudinary.com/hrscywv4p/image/upload/c_limit,h_900,w_1200/njf4aaxacxezk49ooyw2.png)",
     "url(http://res.cloudinary.com/hrscywv4p/image/upload/c_limit,h_900,w_1200/sagxt7wwlqk6cf9j3jhg.jpg)",
-    "url(https://octodex.github.com/images/luchadortocat.png)"
+    "url(https://res.cloudinary.com/hrscywv4p/image/upload/c_limit,f_auto,fl_progressive,h_1500,q_90,w_2000/cyvrmil2ywcjihthwkjb.jpg)"
 ], 
     index = 0;
 
